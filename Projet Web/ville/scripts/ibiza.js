4734f91013881a8526fdab89a0ab8675f6491cf5
Mahé Boissonnet <75388396+420K-alt@users.noreply.github.com>
2025-04-18 21:08:46 +0100

    Update ibiza.js

--- Projet Web/ville/scripts/ibiza.js
+++ Projet Web/ville/scripts/ibiza.js
@@ -68,7 +68,7 @@
     });
 
     payerButton.addEventListener("click", () => {
-        window.location.href = "paiement.html";
+        window.location.href = "../paiement.html";
     });
 
     updateTotal();
